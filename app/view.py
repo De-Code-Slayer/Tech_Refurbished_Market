8234d46f634590bbed48c8ec807c9a5ce7c6957f
emeka <chukwujapheth232@gmail.com>
2022-12-18 12:20:31 +0200

    .

--- app/view.py
+++ app/view.py
@@ -12,6 +12,44 @@ from werkzeug.security import check_password_hash, generate_password_hash
 bp = Blueprint('front', __name__, url_prefix='/')
 
 @bp.route('/')
-
 def home():
-    return render_template('front/base.html')
+    return render_template('front/home.html')
+
+
+
+@bp.route('/services')
+def services():
+    return render_template('front/services-1.html')
+
+
+
+@bp.route('/about')
+def about():
+    return render_template('front/about.html')
+
+
+
+# @bp.route('/news')
+# def news():
+#     return render_template('front/index.html')
+
+
+
+# @bp.route('/quote')
+# def quote():
+#     return render_template('front/index.html')
+
+
+
+# @bp.route('/contact')
+# def contact():
+#     return render_template('front/index.html')
+
+
+
+# @bp.route('/track')
+# def track():
+#     return render_template('front/index.html')
+
+
+

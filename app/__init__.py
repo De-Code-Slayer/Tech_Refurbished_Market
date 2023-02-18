13ace863658ca7de46862abeff2d4896bfe435c1
emeka <chukwujapheth232@gmail.com>
2023-02-18 21:15:21 +0200

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,34 @@
 import os
-
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
+# load env var form .env to env
+load_dotenv()
+
+# create SQL database extension    
+db = SQLAlchemy()
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY = os.getenv('SECRET_KEY'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    # configure the SQLite database, relative to the app instance folder
+    app.config["SQLALCHEMY_DATABASE_URI"]        = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_ECHO']                = os.getenv('SQLALCHEMY_ECHO')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+    
+    # initialize the app with the extension
+    db.init_app(app)
+
+    
+    from .database import sql_db
+    with app.app_context():
+         db.create_all()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
---
+++ app/database/sql_db.py
@@ -0,0 +1,8 @@
+from .. import db
+import datetime 
+
+
+
+# User Profile Object
+class User(db.Model):
+    pass

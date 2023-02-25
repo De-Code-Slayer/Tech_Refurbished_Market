364b11d5a738b5ca57b93245e76931c36844c0c2
emeka <chukwujapheth232@gmail.com>
2023-02-25 20:51:10 +0200

    .

--- app/dashboard_view.py
+++ app/dashboard_view.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -11,10 +11,10 @@ bp = Blueprint('dashboard', __name__, url_prefix='/')
 
 @bp.route('')
 def dashboard():
-    pass
+    return None
 
 
 @bp.route('/dash')
 def dview():
-    
+
     return render_template()
